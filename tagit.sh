782b7093ae47e966d63249bbaeda4ea915115c28
Herb Johnson <herb@zeetogroup.com>
2013-05-29 19:39:58 -0700

    damn tag file

--- tagit.sh
+++ tagit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-exec etags -h ".php" -R \
---exclude="\.git" \
---exclude="\.js" \
---totals=yes \
---tag-relative=yes \
---PHP-kinds=+cf
+# exec ctags -h ".php" -R \
+# --exclude="\.git" \
+# --exclude="\.js" \
+# --totals=yes \
+# --tag-relative=yes \
+# --PHP-kinds=+cf
+
+# ctags -R .
+# find . -name '*.php' -print | xargs etags
+ctags -eR --langmap=php:+.module.install.inc.engine --languages=php
